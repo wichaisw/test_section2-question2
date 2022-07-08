8a3882dcf679ce64add7e17520ecc639d0a611c6
wichaisw <wichai.sw19@gmail.com>
2022-07-08 21:12:16 +0700

    feat: display filtered categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { CategoriesService } from 'src/app/core/services/categories.service';
+import { ICategories } from 'src/app/shared/interfaces/categories';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-question-two';
+  categories: string[] = [] as string[];
+  displayCategories: string[] = [] as string[];
+  filterVal: string = '';
+  categoriesDict: {[key: string]: string } = {};
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) {}
+  
+  ngOnInit(): void {    
+    this.categoriesService.findAll()
+      .subscribe({
+        next: (data: ICategories) => { 
+          for(let category of data.categories) {
+            let lowercaseCategory = category.toLocaleLowerCase();
+            // define dictionary object to capitalize categories later with O(1) time complexity
+            this.categoriesDict[lowercaseCategory] = category;
+            
+            // populate lowercase categories array
+            this.categories.push(lowercaseCategory);
+          }
+          this.displayCategories = this.categories;
+
+        },
+        error: () => console.error('failed to get categories data')
+      });
+  }
+
+  filterCategories(): void {
+    this.displayCategories = this.categories.filter( (cat: string) => {
+      return cat.includes(this.filterVal);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
