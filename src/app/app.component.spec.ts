152fe705aeb6914533b7c4d4bb7e841a44e3b9e0
wichaisw <wichai.sw19@gmail.com>
2022-07-09 00:17:22 +0700

    test: test filter feature

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ICategories } from './shared/interfaces/categories';
+import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  const mockCategories: ICategories = {
+    categories: ['Animal', 'Anime', 'Development', 'Music', 'Open Source Projects', 'Science & Math', 'Weather'],
+    count: 7
+  } as ICategories;
+  const mockLowercaseCategories: string[] = ['animal', 'anime', 'development', 'music', 'open source projects', 'science & math', 'weather'];    
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        SharedModule,
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-question-two'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-question-two');
+  describe('generate dictionary', () => {
+    beforeEach(() => {   
+      spyOn(app, 'generateDictionary').and.callThrough();
+      app.generateDictionary(mockCategories.categories);
+    });
+
+    describe('categoriesDictionaries field', () => {
+      it('should map lowercase categories with original categories', () => {   
+        expect(app.categoriesDict['anime']).toEqual('Anime');
+        expect(app.categoriesDict['science & math']).toEqual('Science & Math');
+        expect(app.categoriesDict['open source projects']).toEqual('Open Source Projects');
+      });
+
+      it('should have same the length with the original categories', () => {
+        expect(Object.keys(app.categoriesDict).length).toEqual(mockCategories.count);
+        expect(Object.keys(app.categoriesDict).length).toEqual(mockCategories.categories.length);
+      });
+    })
+
+    describe('categories field', () => {
+      it('should convert categories to lowercase', () => {
+        expect(app.categories).toEqual(mockLowercaseCategories);
+      });
+
+      it('should have same the length with the original categories', () => {
+        expect(app.categories.length).toEqual(mockCategories.count);
+        expect(app.categories.length).toEqual(mockCategories.categories.length);
+      });
+    }) 
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-question-two app is running!');
+  describe('filter categories', () => {
+    beforeEach(() => {
+      app.categories = mockLowercaseCategories;
+      spyOn(app, 'filterCategories').and.callThrough();
+    });
+
+    it('should update this.displayCategories with filtered lowercase categories', () => {
+      app.filterCategories('an');
+      expect(app.displayCategories).toEqual(['animal', 'anime']);
+      app.filterCategories('AN');
+      expect(app.displayCategories).toEqual(['animal', 'anime']);
+      app.filterCategories('&');
+      expect(app.displayCategories).toEqual(['science & math']);
+      app.filterCategories(' ');
+      expect(app.displayCategories).toEqual(['open source projects', 'science & math']);
+      app.filterCategories('');
+      expect(app.displayCategories).toEqual(app.categories);
+      app.filterCategories('4');
+      expect(app.displayCategories).toEqual([]);
+    });
   });
 });
